5643ac76fe120c5225c9bd499378ccf50f4b43e6
rmallach <mallachr19@gmail.com>
2020-06-03 16:50:49 -0400

    one more step

--- app/routes.py
+++ app/routes.py
@@ -53,13 +53,14 @@ def calibrate_model():
     #excel1 = os.path.join(cwd,'COVID19master/data/COVID_input_parameters.json')
     #test = json.load(open(excel1))
     what = os.listdir(cwd)
+    what1 = os.listdir(os.path.join(cwd, 'app'))
     #q_mat_blank = pd.read_excel(path, sheet_name='Decision')
     # results = COVID_model.run_simulation(state = "NY", decision = rl_input)
     # for k,v in results.items():
     #     results[k].index = results[k].index.astype(str)
     # to_java = {k : json.dumps(v.astype(str).to_dict('index')) for k,v in results.items()}
     #to_java = json.dumps({})
-    to_java = json.dumps({0: cwd, 1:what,}) #2:what})
+    to_java = json.dumps({0: cwd, 1:what, 2:what1}) #2:what})
     return jsonify(status='success', data=to_java)
 
 # @bp.route('/login', methods=['GET', 'POST'])
