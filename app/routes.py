4ea028bdae0f864583a2a60e027de02286307784
rmallach <mallachr19@gmail.com>
2020-06-04 11:39:36 -0400

    lots of changes

--- app/routes.py
+++ app/routes.py
@@ -44,52 +44,45 @@ def download_newfile():
 
 @bp.route('/calibrate_model', methods=('GET','POST'))
 def calibrate_model():
-    #global covid_model
-    #t = time.time()
-    get = request.get_json() # retrieve input data from ajax request
+    get = request.get_json()[0] # retrieve input data from ajax request
     to_df = {}
-    for i,v in enumerate(get):
+    state = get['state']
+    for i,v in enumerate(get['rl_input']):
         to_df[i] = v
     df = pd.DataFrame.from_dict(to_df,orient='index')
     rl_input = read_policy_mod.read_policy(df)
     heroku = False
     if len(os.getcwd()) < 25:
         heroku = True
-    if heroku == False:
-        what = os.listdir(os.path.join(os.getcwd(), 'app\\COVID19master\\data'))
-    else:
-        what = os.listdir(os.path.join(os.getcwd(), 'app/COVID19master/data'))
     covid_model = COVID_model.run_calibration(state='NY', decision=rl_input, heroku=heroku)
     results = COVID_model.run_simulation(covid_model, state = "NY", decision = rl_input, heroku=heroku)
     for k,v in results.items():
         results[k].index = results[k].index.astype(str)
     to_java = {k : json.dumps(v.astype(str).to_dict('index')) for k,v in results.items()}
-    #to_java = json.dumps({'hello':str(len(os.getcwd()))})
-    #print(time.time() - t)
     return jsonify(status='success', data=to_java)
 
-@bp.route('/simulate_model', methods=('GET','POST'))
-def simulate_model(covid_model=covid_model):
-    print(covid_model)
-    t = time.time()
-    get = request.get_json() # retrieve input data from ajax request
-    to_df = {}
-    for i,v in enumerate(get):
-        to_df[i] = v
-    df = pd.DataFrame.from_dict(to_df,orient='index')
-    rl_input = read_policy_mod.read_policy(df)
-    heroku = False
-    if heroku == False:
-        what = os.listdir(os.path.join(os.getcwd(), 'app\\COVID19master\\data'))
-    else:
-        what = os.listdir(os.path.join(os.getcwd(), 'app/COVID19master/data'))
+# @bp.route('/simulate_model', methods=('GET','POST'))
+# def simulate_model(covid_model=covid_model):
+#     print(covid_model)
+#     t = time.time()
+#     get = request.get_json() # retrieve input data from ajax request
+#     to_df = {}
+#     for i,v in enumerate(get):
+#         to_df[i] = v
+#     df = pd.DataFrame.from_dict(to_df,orient='index')
+#     rl_input = read_policy_mod.read_policy(df)
+#     heroku = False
+#     if heroku == False:
+#         what = os.listdir(os.path.join(os.getcwd(), 'app\\COVID19master\\data'))
+#     else:
+#         what = os.listdir(os.path.join(os.getcwd(), 'app/COVID19master/data'))
         
-    results = COVID_model.run_simulation(covid_model, state = "NY", decision = rl_input, heroku=heroku)
-    for k,v in results.items():
-        results[k].index = results[k].index.astype(str)
-    to_java = {k : json.dumps(v.astype(str).to_dict('index')) for k,v in results.items()}
-    print(time.time() - t)
-    return jsonify(status='success', data=to_java)
+#     results = COVID_model.run_simulation(covid_model, state = "NY", decision = rl_input, heroku=heroku)
+#     for k,v in results.items():
+#         results[k].index = results[k].index.astype(str)
+#     to_java = {k : json.dumps(v.astype(str).to_dict('index')) for k,v in results.items()}
+#     print(time.time() - t)
+#     return jsonify(status='success', data=to_java)
 
 # @bp.route('/login', methods=['GET', 'POST'])
 # def login():
--- app/static/userInput-line.js
+++ app/static/userInput-line.js
@@ -3,18 +3,19 @@ function makeUserInputLine(parentName, SVG_name, input_data, dataType='Cases'){
   d3.select("#" + SVG_name).remove()
   // clears the data from the last time
   parentWidth = document.getElementById(parentName).offsetWidth
-  var margin = {top: 25, right: 10, bottom: 20, left: 60},
+  var margin = {top: 25, right: 10, bottom: 20, left: 20},
   height = parentWidth * .75- margin.top - margin.bottom
   width = parentWidth - margin.left - margin.right
 
-  data = input_data
+  data = prep_policy_d3(input_data, dataType)
 
-  x = d3.scaleUtc()
-        .domain(d3.extent(data, d => new Date(d.Date)))
-        .range([0, width - margin.right])
+
+  x = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.x))
+        .range([margin.left, width - margin.right])
 
   y = d3.scaleLinear()
-        .domain([0, d3.max(data, d =>  parseInt(d[dataType]))])
+        .domain([0, d3.max(data, d => d.y)])
         .range([height - margin.bottom, margin.top])
 
   var svg = d3.select("#" + parentName)
@@ -26,19 +27,20 @@ function makeUserInputLine(parentName, SVG_name, input_data, dataType='Cases'){
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   var xAxis = svg.append("g")
-     .attr("transform", "translate(0," + height + ")")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
      .call(d3.axisBottom(x));
 
   var yAxis= svg.append("g")
-     .call(d3.axisLeft(y));
+    .attr("transform", `translate(${margin.left},0)`)
+     .call(d3.axisLeft(y).ticks(5));
 
   drawLine = d3.line()
-               .y(function(d) {return y(d[dataType])})
-               .x(function(d) {return x(new Date(d.Date))});
+               .y(function(d) {return y(d.y)})
+               .x(function(d) {return x(d.x)});
 
   // Add the line
   svg.append("path")
-     .datum(input_data)
+     .datum(data)
      .attr('class', 'line')
      .attr("fill", "none")
      .attr("stroke", "steelblue")
@@ -52,4 +54,36 @@ function makeUserInputLine(parentName, SVG_name, input_data, dataType='Cases'){
     .style("font-size", "14px")
     .style("text-decoration", "underline")
     .text(dataType);
+}
+
+
+
+function prep_policy_d3(input_data, dataType){
+  if (dataType=='Contact Reduction'){
+    data = {'x':input_data['Startday(ContactReduction)'],
+            'y':input_data['ContactReduction(0to1)']}
+  }else if (dataType=='Contact Tracing'){
+    data = {'x':input_data['Startday(ContactTracing)'],
+            'y':input_data['Testsperday(ContactTracing)']}
+  }else if (dataType=='Universal Testing'){
+    data = {'x':input_data['Startday(UniversalTesting)'],
+            'y':input_data['Testsperday(UniversalTracing)']}
+  }
+
+  data['x'] = data['x'].filter(function (value) {
+      return !Number.isNaN(value);
+  });
+  data['y'] = data['y'].filter(function (value) {
+      return !Number.isNaN(value);
+  });
+  num_entries = data.x.length
+
+  new_data = []
+  for (var i=1; i<num_entries; i++){
+    span = data['x'][i] - data['x'][i-1]
+    for (var d=0; d<span; d++){
+      new_data[new_data.length] = {'x':new_data.length, 'y':data['y'][i-1]}
+    }
+  }
+  return new_data
 }
