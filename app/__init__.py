57c2e0bc09b2c65bee8e7e5149b8a8612bd967c5
rmallach <mallachr19@gmail.com>
2020-07-14 14:43:00 -0400

    post cleanse

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,8 @@
 import os
 from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
-#from flask_migrate import Migrate
-#from flask_login import LoginManager
 from flask_assets import Bundle, Environment
 
-#db = SQLAlchemy()
-#migrate = Migrate()
+
 
 bundles = {
         'd3_viz':Bundle('map.js', 'foresight.js', 'covid-line.js',
@@ -22,15 +18,7 @@ ALLOWED_EXTENSIONS = {'csv', 'xls', 'xlsx'}
 
 def create_app():
     app = Flask(__name__)
-    # app.config.from_mapping(
-    #     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
-    #     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #         'sqlite:///' + os.path.join(app.instance_path, 'app.sqlite'),
-    #     SQLALCHEMY_TRACK_MODIFICATIONS = False,
-    # )
 
-    #db.init_app(app)
-    #migrate.init_app(app, db)
 
     assets = Environment(app)
     assets.register(bundles)
